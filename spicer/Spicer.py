f78cf6e1f7d2e7ca0bccee5bbada4a06d2da708a
ItsTato <itstato.official@gmail.com>
2024-09-22 01:06:02 -0600

    oops wrong place for git .

--- spicer/Spicer.py
+++ spicer/Spicer.py
@@ -1,22 +1,76 @@
 from re import findall
-from flask import Flask
-from os import path
+from flask import Flask, render_template_string
+from os import path, listdir
+from typing import Any
+
+from .Template import Template
+from .Spice import Spice
 
 class Spicer:
-	def __init__(self,app:Flask) -> None:
+	def __init__(self,app:Flask,spicer_folder:str|None="spices") -> None:
 		self.__app:Flask = app
-	
-	def patch(self,rendered:str) -> str:
+		self.__spicer_folder:str = spicer_folder if spicer_folder is not None else path.join(str(self.__app.template_folder),path.join("..","spices"))
+		self.__pattern:str = r"&<([a-zA-Z_][a-zA-Z0-9_]*)\.([a-zA-Z_][a-zA-Z0-9_]*)>" # &<{parent}.{child}>
+
+	def __get_spices__(self,_path:str|None=None) -> list[str]:
+		if _path is None: _path = self.__spicer_folder
+		if not path.exists(_path): raise Exception("No \"spicer\" folder was found.")
+		spices:list[str] = []
+		for folder in listdir(_path):
+			spices.append(folder)
+		return spices
+
+	def load_spice(self,location:str,name:str) -> Spice:
+		spice_folder:str = path.join(location,name)
+		spice:Spice = Spice(name)
+		with open(path.join(spice_folder,".html"),"r") as file:
+			spice.HTML_File = file.read()
+		if path.exists(path.join(spice_folder,".css")):
+			with open(path.join(spice_folder,".css"),"r") as file:
+				spice.CSS_File = file.read()
+		if path.exists(path.join(spice_folder,".pre.js")):
+			with open(path.join(spice_folder,".pre.js"),"r") as file:
+				spice.JS_PreRender_File = file.read()
+		if path.exists(path.join(spice_folder,".aft.js")):
+			with open(path.join(spice_folder,".aft.js"),"r") as file:
+				spice.JS_After_File = file.read()
+		return spice
+
+	def render_template(self,template_name:str,**context:Any) -> str:
+		"""
+		Renders & patches a template.
+
+		:param template_name: The name of the template to render.
+		:param context: The variables to make available in the template.
+		"""
+
+		template_folder:str = path.join(self.__app.template_folder,template_name)#type:ignore
+
+		with open(path.join(template_folder,".html"),"r") as file:
+			text = file.read()
+			html_template:str = render_template_string(text,*context)
+
+		return html_template
+
+	def patch(self,rendered:str,**context:Any) -> str:
 		"""
 		Patch up an already-rendered template using Spicer.
 
 		:param rendered: The rendered template
+		:param context: Any context to pass into the spices
 		"""
 
-		pattern:str = r"&<([^>]+)>"
-		constants:list[str] = findall(pattern,rendered)
+		constants:list[tuple[str,str]] = findall(self.__pattern,rendered)
 
 		for constant in constants:
-			pass#continue working later
+			spice:Spice = self.load_spice(*constant)
+			spice.HTML_File = render_template_string(spice.HTML_File,*context)
+			rendered = rendered.replace(f"&<{constant[0]}.{constant[1]}>",spice.HTML_File)
+			if spice.CSS_File != None:
+				rendered = rendered.replace("</head>",f"<style>{spice.CSS_File}</style></head>")
+			if spice.JS_PreRender_File != None:
+				rendered = rendered.replace("<body>",f"<body><script>{spice.JS_PreRender_File}</script>")
+			if spice.JS_After_File != None:
+				rendered = rendered.replace("</body>",f"<script>{spice.JS_After_File}</script></body>")
 
-		return ""
+		return rendered
--- spicer/cli.py
+++ spicer/cli.py
@@ -1,10 +1,11 @@
 from colorama import just_fix_windows_console, Fore
 
+def comment(content:str) -> str:
+	return f"{Fore.LIGHTBLACK_EX}# {content}{Fore.RESET}"
+minus:str = f"{Fore.LIGHTYELLOW_EX}-{Fore.RESET}"
+
 def run_help(command_name:str) -> None:
 	if command_name == "all":
-		minus:str = f"{Fore.LIGHTYELLOW_EX}-{Fore.RESET}"
-		def comment(content:str) -> str:
-			return f"{Fore.LIGHTBLACK_EX}// {content}{Fore.RESET}"
 		print()
 		print(f"Spicer commands list")
 		print()
@@ -12,7 +13,20 @@ def run_help(command_name:str) -> None:
 		print(f"{minus} spicer setup <project name> {comment('Sets up a new project')}")
 		print()
 		return
-	return
+	if command_name == "help":
+		print()
+		print(f"{Fore.LIGHTYELLOW_EX}help <command name> {Fore.RESET}| Command Information")
+		print()
+		print(f"Gets information on a command (the <command name> argument). By passing \"all\" as the <command name> a list of every command will be generated.")
+		print()
+		return
+	if command_name == "setup":
+		print()
+		print(f"{Fore.LIGHTYELLOW_EX}setup <project name> {Fore.RESET}| Command Information")
+		print()
+		print(f"Sets up a new project. The <project name> argument is the name of the project.")
+		print()
+		return
 
 def run_setup(project_name:str) -> None:
 	return
@@ -31,7 +45,7 @@ def run(argc:int,argv:list[str]) -> None:
 	if argv[1] == "help":
 		if argc < 3:
 			print()
-			print(f"Nuh uh. You {Fore.LIGHTRED_EX}must{Fore.RESET} a {Fore.LIGHTYELLOW_EX}command name{Fore.RESET}!")
+			print(f"Nuh uh. You {Fore.LIGHTRED_EX}must{Fore.RESET} provide a {Fore.LIGHTYELLOW_EX}command name{Fore.RESET}!")
 			print()
 			return
 		run_help(argv[2])
@@ -39,7 +53,7 @@ def run(argc:int,argv:list[str]) -> None:
 	if argv[1] == "setup":
 		if argc < 3:
 			print()
-			print(f"Nuh uh. You {Fore.LIGHTRED_EX}must{Fore.RESET} a {Fore.LIGHTYELLOW_EX}project name{Fore.RESET}!")
+			print(f"Nuh uh. You {Fore.LIGHTRED_EX}must{Fore.RESET} provide a {Fore.LIGHTYELLOW_EX}project name{Fore.RESET}!")
 			print()
 			return
 		run_setup(argv[2])
