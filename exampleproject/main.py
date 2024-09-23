260a523cd1f748700c1eec9bb35ed6c4fc628057
ItsTato <itstato.official@gmail.com>
2024-09-22 18:12:01 -0600

    finalize example

--- exampleproject/main.py
+++ exampleproject/main.py
@@ -6,9 +6,17 @@ site:Flask = Flask(__name__)
 spicer:Spicer = Spicer(site)
 
 @site.route("/")
-def index() -> str:
+def index():
 	# Any of the two following ways renders & patches the template + any spices
 	#return spicer.patch( flask.render_template("index.html") )
 	return spicer.render_template("index")
+
+@site.route("/home")
+def home():
+	return spicer.render_template("home")
+
+@site.route("/about")
+def about():
+	return spicer.render_template("about")
 
 site.run(host="0.0.0.0",port=2323)
